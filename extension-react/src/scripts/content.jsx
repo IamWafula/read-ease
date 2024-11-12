2a104880f1ff0e4f2aa0bfc2b61e6ea168f391c0
Ian Wafula <46840113+IamWafula@users.noreply.github.com>
2024-11-11 22:40:05 -0300

    created-react-version

---
+++ extension-react/src/scripts/content.jsx
@@ -0,0 +1,6 @@
+const root = document.createElement("div");
+root.id = "crx-root";
+document.body.appendChild(root);
+
+// to show that the content script is working
+document.body.style.backgroundColor = "black";
