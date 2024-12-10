83333eb8c4edc699a793cf6213e97d41ad030516
Ian Wafula <46840113+IamWafula@users.noreply.github.com>
2024-12-10 11:51:01 -0300

    added docker file

---
+++ backend/asgi.py
@@ -0,0 +1,35 @@
+# Asgi file to run the flask app using uvicorn server
+from asgiref.wsgi import WsgiToAsgi
+import uvicorn
+
+# flask app imports
+from flask import Flask
+from routes.text_processing import text_processing_bp
+from config import Config
+from flask_cors import CORS
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app, supports_credentials=True)
+    app.config.from_object(Config)
+
+    limiter = Limiter(
+        app,
+        default_limits=["1 per minute"],
+    )
+
+    # Register the blueprint for text processing
+    app.register_blueprint(text_processing_bp)
+
+    return app
+
+
+app = create_app()
+asgi_app = WsgiToAsgi(app)
+
+if __name__ == "__main__":
+    uvicorn.run(asgi_app, host="0.0.0.0", port=3000, log_level="info")
