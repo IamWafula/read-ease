f7bc6e14ab3cc6d368e97ab51984def6b8819dae
Ian Wafula <46840113+IamWafula@users.noreply.github.com>
2024-12-11 02:17:26 -0300

    added boilerplate for easier integration

--- read-ease-app/src/pages/login/login.jsx
+++ read-ease-app/src/pages/login/login.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import { auth } from './firebase';
-import { signInWithCredential, GoogleAuthProvider } from 'firebase/auth';
+import { auth, provider } from './firebase';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { clientId } from './config';
 
 export default function Login (){
     const handleLogin = async () => {
-        const provider = new GoogleAuthProvider();
-        try {
-            const result = await auth.signInWithPopup(provider);
-            // User signed in
-            console.log(result.user);
-        } catch (error) {
-            // Handle Errors
-            console.error('Auth error:', error);
-        }
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
     };
 
   return <button onClick={handleLogin}>Sign in with Google</button>;
