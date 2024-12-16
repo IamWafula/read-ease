7a48629a1b1eab96bbf5ea2bcacd0965584d9b4f
Ian Wafula <46840113+IamWafula@users.noreply.github.com>
2024-12-16 18:48:27 -0300

    changes for the deployment

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -9,6 +9,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 import json
+from bson.objectid import ObjectId
 
 
 user_bp = Blueprint("user", __name__)
@@ -16,51 +17,95 @@ user_bp = Blueprint("user", __name__)
 
 def get_mongo_client():
     if os.getenv("ENV") == "production":
-        pass
+        return AsyncIOMotorClient(
+            os.getenv("MONGO_URI"), tlsAllowInvalidCertificates=True
+        )
     else:
-        return AsyncIOMotorClient(os.getenv("MONGO_URI"))
+        return AsyncIOMotorClient(
+            os.getenv("MONGO_URI"), tlsAllowInvalidCertificates=True
+        )
 
 
 @user_bp.route("/add_document", methods=["POST"])
 @async_authorization_required()
 async def add_document():
-    data = request.get_json()
-    user_id = data["uid"]
+    try:
+        data = request.get_json()
+        user_id = data["uid"]
 
-    mong_client = get_mongo_client()
+        mong_client = get_mongo_client()
 
-    db = mong_client.get_database("readEase")
-    collection = db.get_collection("documents")
+        db = mong_client.get_database("readEase")
+        collection = db.get_collection("documents")
 
-    document = {
-        "title": "Untitled Document",
-        "text": "",
-        "user_id": user_id,
-        "keywords": [],
-        "sentences": [],
-    }
+        document = {
+            "title": "Untitled Document",
+            "text": "",
+            "user_id": user_id,
+            "keywords": [],
+            "sentences": [],
+        }
 
-    result = await collection.insert_one(document)
+        result = await collection.insert_one(document)
 
-    print(result)
+        document_id = result.inserted_id
+        document_id = str(document_id)
 
-    return jsonify({"document_id": str(50)}), 200
+        return jsonify({"document_id": document_id}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "An error occurred"}), 500
+
+
+@user_bp.route("/get_document", methods=["POST"])
+@async_authorization_required()
+async def get_document_by_id():
+    try:
+        data = request.get_json()
+        user_id = data["uid"]
+        document_id = data["document_id"]
+
+        mong_client = get_mongo_client()
+
+        db = mong_client.get_database("readEase")
+        collection = db.get_collection("documents")
+
+        document = await collection.find_one(
+            {"user_id": user_id, "_id": ObjectId(document_id)}
+        )
+
+        if document is None:
+            return jsonify({"error": "Document not found"}), 404
+
+        document["_id"] = str(document["_id"])
+
+        return jsonify(document), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "An error occurred"}), 404
 
 
 @user_bp.route("/get_documents", methods=["POST"])
 @async_authorization_required()
 async def get_documents():
-    data = request.get_json()
-    user_id = data["uid"]
 
-    mong_client = get_mongo_client()
+    try:
+        data = request.get_json()
+        user_id = data["uid"]
 
-    db = mong_client.get_database("readEase")
-    collection = db.get_collection("documents")
+        print(os.getenv("MONGO_URI"))
 
-    documents = await collection.find({"user_id": user_id}).to_list(length=None)
+        mong_client = get_mongo_client()
 
-    return json.loads(json.dumps(documents, default=str))
+        db = mong_client.get_database("readEase")
+        collection = db.get_collection("documents")
+
+        documents = await collection.find({"user_id": user_id}).to_list(length=None)
+
+        return json.loads(json.dumps(documents, default=str))
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "An error occurred"}), 404
 
 
 @user_bp.route("/delete_document", methods=["POST"])
