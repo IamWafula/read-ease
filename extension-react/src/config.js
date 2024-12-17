d1b355f308e3b82a9a3cea1771bcc9f1001f2d08
Ian Wafula <46840113+IamWafula@users.noreply.github.com>
2024-12-16 23:25:24 -0300

    config file for running extension locally

---
+++ extension-react/src/config.js
@@ -0,0 +1,23 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC47y3u6oM8P41MRMu7qyC6e6dn1oSpJt8",
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC47y3u6oM8P41MRMu7qyC6e6dn1oSpJt8",
+    authDomain: "chrome-extension://lbdjpnhpccfepeddmcfjcpmagijkpojb",
+    projectId: "cs162readease",
+    storageBucket: "cs162readease.firebasestorage.app",
+    messagingSenderId: "763128562280",
+    appId: "1:763128562280:web:e405157c9c3e985c04b1dd",
+    measurementId: "G-SH2YH90EST"
+};
+
+const clientId = "763128562280-rfoqnj6cul904pm54qls38839anmecvg.apps.googleusercontent.com";
+
+export { firebaseConfig, clientId };
